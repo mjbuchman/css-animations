f311d2f781e5c9d756acebecab615192c57f6598
Michael Buchman <mbookie213@gmail.com>
2018-07-16 16:02:01 -0500

    Added sidebar menu

--- menu.js
+++ menu.js
@@ -1,1 +1,22 @@
+const menuButton = document.querySelector("#menu");
+const menuArrow = document.querySelector("#menu img");
+const menu = document.querySelector(".menu");
+const sidenav = document.querySelector(".sidenav");
 
+var count = 0;
+
+function menuToggle(){
+    if(count%2==0){
+        sidenav.style.width = "50px";
+        menu.style.right = "50px";
+        menuArrow.style.transform = "rotate(180deg)";
+    } else{
+        sidenav.style.width = "0";
+        menu.style.right = "0";
+        menuArrow.style.transform = "rotate(0deg)";
+    }
+    count++;
+}
+
+
+menuButton.addEventListener("click", menuToggle, false);
