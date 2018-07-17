4561188235d2beebdcc2c3e0cabf94880c253c56
Michael Buchman <mbookie213@gmail.com>
2018-07-17 13:14:18 -0500

    Finished sidebar menu

--- menu.js
+++ menu.js
@@ -2,11 +2,19 @@ const menuButton = document.querySelector("#menu");
 const menuArrow = document.querySelector("#menu img");
 const menu = document.querySelector(".menu");
 const sidenav = document.querySelector(".sidenav");
+const githubButton = document.querySelector("#github");
+const linkedinButton = document.querySelector("#linkedinMJB");
+const portfolioButton = document.querySelector("#portfolio");
+const infoButton = document.querySelector("#info");
+const infoTextBox = document.querySelector("#info-text");
+const infoText = document.querySelector("#info-text p");
 
-var count = 0;
+
+var menuCount = 0;
+var infoCount = 0;
 
 function menuToggle(){
-    if(count%2==0){
+    if(menuCount%2==0){
         sidenav.style.width = "50px";
         menu.style.right = "50px";
         menuArrow.style.transform = "rotate(180deg)";
@@ -14,9 +22,39 @@ function menuToggle(){
         sidenav.style.width = "0";
         menu.style.right = "0";
         menuArrow.style.transform = "rotate(0deg)";
+        infoTextBox.style.width = '0';
+        infoText.style.visibility = "hidden";
+        infoCount++;
     }
-    count++;
+    menuCount++;
+}
+
+function visitGithub(){
+    window.location = 'https://github.com/mjbuchman/css-animations';
+}
+
+function visitLinkedIn(){
+    window.location = 'https://www.linkedin.com/in/mjbuchman/';
+}
+
+function visitPortfolio(){
+    window.location = 'https://mbuchman.carbonmade.com/';
+}
+
+function displayInfo(){
+    if(infoCount%2==0){
+        infoTextBox.style.width = "240px";
+        infoText.style.visibility = "visible";
+    } else{
+        infoTextBox.style.width = '0';
+        infoText.style.visibility = "hidden";
+    }
+    infoCount++;
 }
 
 
 menuButton.addEventListener("click", menuToggle, false);
+githubButton.addEventListener("click", visitGithub, false);
+linkedinButton.addEventListener("click", visitLinkedIn, false);
+portfolioButton.addEventListener("click", visitPortfolio, false);
+infoButton.addEventListener("click", displayInfo, false);
