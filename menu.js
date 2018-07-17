b9a0186075ad0e6f01274807ef959c16090ff287
Michael Buchman <mbookie213@gmail.com>
2018-07-17 16:00:49 -0500

    Changed menu buttons

--- menu.js
+++ menu.js
@@ -2,16 +2,11 @@ const menuButton = document.querySelector("#menu");
 const menuArrow = document.querySelector("#menu img");
 const menu = document.querySelector(".menu");
 const sidenav = document.querySelector(".sidenav");
-const githubButton = document.querySelector("#github");
-const linkedinButton = document.querySelector("#linkedinMJB");
-const portfolioButton = document.querySelector("#portfolio");
 const infoButton = document.querySelector("#info");
 const infoTextBox = document.querySelector("#info-text");
 const infoText = document.querySelector("#info-text p");
 
-
 var menuCount = 0;
-var infoCount = 0;
 
 function menuToggle(){
     if(menuCount%2==0){
@@ -29,18 +24,6 @@ function menuToggle(){
     menuCount++;
 }
 
-function visitGithub(){
-    window.location = 'https://github.com/mjbuchman/css-animations';
-}
-
-function visitLinkedIn(){
-    window.location = 'https://www.linkedin.com/in/mjbuchman/';
-}
-
-function visitPortfolio(){
-    window.location = 'https://mbuchman.carbonmade.com/';
-}
-
 function displayInfo(){
     if(infoCount%2==0){
         infoTextBox.style.width = "240px";
@@ -52,9 +35,5 @@ function displayInfo(){
     infoCount++;
 }
 
-
 menuButton.addEventListener("click", menuToggle, false);
-githubButton.addEventListener("click", visitGithub, false);
-linkedinButton.addEventListener("click", visitLinkedIn, false);
-portfolioButton.addEventListener("click", visitPortfolio, false);
 infoButton.addEventListener("click", displayInfo, false);
